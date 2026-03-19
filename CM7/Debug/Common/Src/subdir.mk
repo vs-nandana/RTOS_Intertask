0ea82c25ffaa9513feb9750a06faf166d9c16bf2
Nandana V S <v.nandana.kumar@gmail.com>
2026-03-19 10:36:44 +0530

    [Development] Refs#xxx: Change task Priorities
    [Description]:
    	> Changed freeRTOS task priorities

--- CM7/Debug/Common/Src/subdir.mk
+++ CM7/Debug/Common/Src/subdir.mk
@@ -5,7 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/home/nandana/CCodesSTM/STMH755_RTOS_taskComm/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.c 
+/home/nandana/CCodesSTM/RTOS_IntertaskFinal/RTOS_Intertask/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.c 
 
 OBJS += \
 ./Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.o 
@@ -15,7 +15,7 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.o: /home/nandana/CCodesSTM/STMH755_RTOS_taskComm/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.c Common/Src/subdir.mk
+Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.o: /home/nandana/CCodesSTM/RTOS_IntertaskFinal/RTOS_Intertask/Common/Src/system_stm32h7xx_dualcore_boot_cm4_cm7.c Common/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I../Core/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../../Middlewares/Third_Party/FreeRTOS/Source/include -I../../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Common-2f-Src
